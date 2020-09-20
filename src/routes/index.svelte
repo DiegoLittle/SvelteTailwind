e27320948bd7f19aedbb924e0ee9aba8c1fa3cf6
DiegoLittle <diegochelittle@gmail.com>
2020-09-19 23:24:39 -0500

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<style>
+  .btn {
+    @apply bg-red-500 text-red-100 uppercase tracking-wide font-semibold
+      text-4xl px-4 py-3 shadow-lg rounded;
+  }
+</style>
+
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<div class="space-y-10 text-center">
+  <h1 class="text-7xl uppercase font-bold">Great success!</h1>
+  <button class="btn">DO NOT PRESS THIS BUTTON!</button>
+</div>
